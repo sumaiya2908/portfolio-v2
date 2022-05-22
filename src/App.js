36fdd120fbbf2ed140e4859d4a23f0bebec64294
unknown <sumaiyahas2908@gmail.com>
2022-05-22 15:55:05 +0530

    refactor: remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { Box, Container } from '@mui/material';
+import { Box } from '@mui/material';
 import React from 'react';
-import About from './components/About';
 import Elements from './components/Elements';
 import Home from './components/Home';
-import NavBar from './components/NavBar';
-import Projects from './components/Projects';
 
 function App() {
   return (
