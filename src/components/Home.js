40dc3618a1291c9f04f0a489d886dd0f06eea735
unknown <sumaiyahas2908@gmail.com>
2022-08-30 22:21:20 +0530

    refactor: make SM icons small

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,13 @@ import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineInstagram, AiOutlineDribbb
 function Home() {
   return (
     <Box className='box gray-bg large-text justify-center fullview-box'>
-      <Box>Hello, I'm <span className='colored-text cursive'>Summayya</span>.</Box>
-      <Box>A <span className='colored-text'>Software Developer</span> based in India.</Box>
+      <Box className="left-animate">Hello, I'm <span className='colored-text cursive'>Summayya</span>.</Box>
+      <Box className="right-animate">A <span className='colored-text'>Software Developer</span> based in India.</Box>
       <Box marginTop={3.5}>
-        <AiOutlineGithub className='icon social-icon'/>
-        <AiOutlineLinkedin className='icon social-icon'/>
-        <AiOutlineInstagram className='icon social-icon'/>
-        <AiOutlineDribbble className='icon'/>
+        <AiOutlineGithub className='icon social-icon small-icon'/>
+        <AiOutlineLinkedin className='icon social-icon small-icon'/>
+        <AiOutlineDribbble className='icon social-icon'/>
+        <AiOutlineInstagram className='icon'/>
       </Box>
     </Box>
   )
