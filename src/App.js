13e11574c13becc099ff56e498cd2f168e96f307
unknown <sumaiyahas2908@gmail.com>
2022-09-02 00:05:17 +0530

    refactor: add ract routers

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import { Box } from '@mui/material';
 import React from 'react';
 import Elements from './components/Elements';
 import Home from './components/Home';
-
+import { BrowserRouter, Route, Routes, Ind } from 'react-router-dom';
+import Testani from './components/testani';
+import CodingBook from './components/CodingBook';
 function App() {
-  return (
-    <Box>
-      <Home />
-      <Elements/>
-    </Box>
-  )
+	return (
+		<BrowserRouter>
+			<Box>
+
+				<Routes>
+					<Route exact path="/" element={<Home />}> </Route>
+					<Route path="/coding-book" element={<CodingBook />}></Route>
+				</Routes>
+			</Box>
+
+
+		</BrowserRouter>
+
+	)
 }
 
 export default App
