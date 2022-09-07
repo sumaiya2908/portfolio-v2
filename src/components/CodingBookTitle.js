3575055b084610f174de855e7af764ddead11f0e
unknown <sumaiyahas2908@gmail.com>
2022-09-07 23:08:55 +0530

    feat: add react markdown

--- src/components/CodingBookTitle.js
+++ src/components/CodingBookTitle.js
@@ -3,10 +3,11 @@ import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import config from '../config';
 import { Box, CircularProgress } from '@mui/material';
+import ReactMarkdown from 'react-markdown';
 
 function CodingBookTitle() {
   const { title } = useParams();
-  const [note, setNote] = useState();
+  const [note, setNote] = useState([]);
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -14,14 +15,15 @@ function CodingBookTitle() {
       `https://strapi-backend-portfoliov2.herokuapp.com/api/code-books?filters[title][$eq]=${title}`,
       config
     )
-      .then((response) => { setNote(response.data.data); setLoading(false) })
+      .then((response) => { setNote(response.data.data[0]); setLoading(false) })
       .catch((err) => console.log(err))
 
   }, [])
+console.log(note.attributes)
   return (
-    // {loading ? <CircularProgr> :
-    <Box className="box bg-gray">CodingBookTitle
-    <CircularProgress/>
+    <Box className="box bg-gray fullview-box">
+    {loading? <CircularProgress className='loader'/>: null} 
+    <ReactMarkdown>{note.attributes? note.attributes.notes : null}</ReactMarkdown>
     </Box>
   )
 }
