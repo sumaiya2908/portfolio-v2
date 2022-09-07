e1274ba7ff883a978bfc1de83828054491d7575a
unknown <sumaiyahas2908@gmail.com>
2022-09-07 20:25:26 +0530

    refactor: add link to book

--- src/components/CodingBook.js
+++ src/components/CodingBook.js
@@ -5,39 +5,45 @@ import moment from 'moment';
 
 import config from '../config';
 import NavBar from './NavBar';
+import { Link } from 'react-router-dom';
 
 function CodingBook() {
 	const [notes, setNotes] = useState([]);
 
 	useEffect(() => {
-		axios.get( 
+		axios.get(
 			'https://strapi-backend-portfoliov2.herokuapp.com/api/code-books',
 			config
 		)
-		.then((response) => setNotes(response.data.data))
-		.catch((err) => console.log(err))
+			.then((response) => setNotes(response.data.data))
+			.catch((err) => console.log(err))
 
 	}, [])
 
 	return (
-		<Box className='box gray-bg layout-padding fullview-box notes-container'>
-		<NavBar/>
-		{notes.map(({attributes}, i) => {
-			return(
-				<Box className='note-container border-b border-secondary' key={i}>
-					<Box className='note-title'>
-					{attributes.title}
-					</Box>
-					<Box className='note-desc'>
-					{attributes.description}
-					</Box>
-					<Box className='note-date'>
-					{moment(attributes.createdAt).format("MMM Do YYYY")}
-					</Box>
-				</Box>
-			)
-		})}
-			
+		<Box className="coding-book box gray-bg">
+			<NavBar />
+			<Box className='box gray-bg layout-padding fullview-box notes-container'>
+
+				{notes.map(({ attributes }, i) => {
+					return (
+						<Box className='note-container border-b border-secondary' key={i}>
+							<Link to={'/coding-book/' + attributes.title}>
+								<Box className='note-title'>
+									{attributes.title}
+								</Box>
+							</Link>
+							<Box className='note-desc'>
+								{attributes.description}
+							</Box>
+							<Box className='note-date'>
+								{moment(attributes.createdAt).format("MMM Do YYYY")}
+							</Box>
+						</Box>
+					)
+				})}
+
+			</Box>
 		</Box>
 	)
 }
