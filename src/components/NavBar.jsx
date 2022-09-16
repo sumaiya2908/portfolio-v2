33337d2d3f9f223be076d3cff6075fe919e2750b
unknown <sumaiyahas2908@gmail.com>
2022-09-16 13:11:13 +0530

    refactor: rename file from js to jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,17 @@
-import { Box } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom';
 
 function NavBar() {
 	const NavItems = ['Home', 'About', 'Projects', 'CodeBook', 'Resume'];
 	return (
-		<Box className='nav-container'>
+		<div className='nav-container'>
 			{NavItems.map((Item, i) => {
-				return (<Box className='nav-item' key={i}>
+				return (<div className='nav-item' key={i}>
 					<Link to={`\/${Item.toLowerCase()}`}>{Item}</Link>
-				</Box>)
+				</div>)
 			})}
 
-		</Box>
+		</div>
 	)
 }
 
