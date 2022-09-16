9afbcef79c39e68ac26da6ca252030d17c2d7b90
unknown <sumaiyahas2908@gmail.com>
2022-09-16 13:09:55 +0530

    refactor: rename file from js to jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { AiOutlineGithub, AiOutlineLinkedin} from 'react-icons/ai'
+import {SiHashnode} from 'react-icons/si'
+
+import NavBar from './NavBar'
+
+function Home() {
+  return (
+    <div className=''>
+      <div className=''>Hello, I'm <span className='colored-text'>&#60;Sumaiya/&#62;</span>.</div>
+      <div className=''>A <span className='colored-text'>Software Developer</span> based in India.</div>
+      <div>
+        <a href='https://github.com/sumaiya2908' target='_blank'><AiOutlineGithub className='icon social-icon small-icon' /></a>
+        <a href='https://www.linkedin.com/in/summayyahasmani/' target='_blank'><AiOutlineLinkedin className='icon social-icon small-icon'/></a>
+        <a href='https://sumaiya.hashnode.dev/' target='_blank'><SiHashnode className='icon social-icon small-icon'/></a>
+      </div>
+    </div>
+  )
+}
+
+export default Home
