f74a5048e848cb1160359a2799b90ae6a7d50665
unknown <sumaiyahas2908@gmail.com>
2022-09-16 13:10:31 +0530

    refactor: rename file from js to jsx

--- src/components/CodingBook.jsx
+++ src/components/CodingBook.jsx
@@ -1,4 +1,3 @@
-import { Box, CircularProgress } from '@mui/material'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
@@ -9,42 +8,42 @@ import { Link } from 'react-router-dom';
 
 function CodingBook() {
 	const [notes, setNotes] = useState([]);
-	const[loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
 		axios.get(
 			'https://strapi-backend-portfoliov2.herokuapp.com/api/code-books',
 			config
 		)
-			.then((response) => {setNotes(response.data.data); setLoading(false)})
+			.then((response) => { setNotes(response.data.data); setLoading(false) })
 			.catch((err) => console.log(err))
 	}, [])
 
 	return (
-		<Box className='coding-book box gray-bg'>
+		<div className='coding-book box gray-bg'>
 			<NavBar />
-			<Box className='box gray-bg layout-padding fullview-box notes-container'>
+			<div className='box gray-bg layout-padding fullview-box notes-container'>
 				{loading ?
-				<CircularProgress className='loader'/> : null}
+					<div></div>:null}
 				{notes.map(({ attributes }, i) => {
 					return (
-						<Box className='note-container border-b border-secondary' key={i}>
+						<div className='note-container border-b border-secondary' key={i}>
 							<Link to={'/codebook/' + attributes.title}>
-								<Box className='note-title'>
+								<div className='note-title'>
 									{attributes.title}
-								</Box>
+								</div>
 							</Link>
-							<Box className='note-desc'>
+							<div className='note-desc'>
 								{attributes.description}
-							</Box>
-							<Box className='note-date'>
+							</div>
+							<div className='note-date'>
 								{moment(attributes.createdAt).format('MMM Do YYYY')}
-							</Box>
-						</Box>
+							</div>
+						</div>
 					)
 				})}
-			</Box>
-		</Box>
+			</div>
+		</div>
 	)
 }
 
