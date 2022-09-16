597b8c66dd5df37ce61a691d1ce695cd5e0f42f1
unknown <sumaiyahas2908@gmail.com>
2022-09-16 13:10:02 +0530

    refactor: rename file from js to jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+function About() {
+    return (
+        <div className='box layout-padding'>
+            <div className='border-bottom large-text'>
+                About me
+            </div>
+        </div>
+    )
+}
+
+export default About
