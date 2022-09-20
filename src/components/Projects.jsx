39dbe69b6486b0785e4a38a64951d6a331565e29
unknown <sumaiyahas2908@gmail.com>
2022-09-20 13:57:15 +0530

    feat: add loader

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,43 +1,65 @@
 import React, { useState, useEffect } from 'react'
-import { AiOutlineGithub, AioutlineE } from 'react-icons/ai'
+import { AiOutlineGithub } from 'react-icons/ai'
 import { FiExternalLink } from 'react-icons/fi'
 import axios from 'axios'
 
 import config from '../config';
+import NavBar from './NavBar';
+import Loader from './Loader';
+
 function Projects() {
     const [projects, setProjects] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         axios.get(
             'https://strapi-backend-portfoliov2.herokuapp.com/api/projects',
             config
         )
-            .then((response) => setProjects(response.data.data))
+            .then((response) => { setProjects(response.data.data); setLoading(false) })
             .catch(console.log);
     }, [])
 
     return (
-        <div className='box grey-bg layout-padding'>
+        <>
+            <NavBar />
 
-            <div className='border-bottom large-text'>
-                Projects
-            </div>
-            <div className='project-container layout-padding'>
-                {projects.map(({ attributes }, i) => {
-                    return (<Card variant='outlined' className='card' key={i}>
-                        <CardHeader title={attributes.title} />
-                        <CardContent>
-                            {attributes.description}
-                        </CardContent>
-                        <CardActions>
-                            {attributes.github ? <a href={attributes.github} target='_blank'><AiOutlineGithub className='icon' /></a> : null}
-                            {attributes.web ? <a href={attributes.web} target='_blank'><FiExternalLink className='icon' /></a> : null}
-                        </CardActions>
-                    </Card>)
-                })}
+            <div className='container'>
+                <div className='heading colored-text large-text typing'>
+                    Projects.
+                </div>
+                {loading ? <Loader /> : null}
+                <div className='project-container layout-padding'>
+                    {projects.map(({ attributes }, i) => {
+                        return (
+                            <div className='project-container' key={i}>
+                                <div className='project-card'>
 
+                                    <div className='project-title'>
+                                        {attributes.title}
+                                    </div>
+
+                                    <div className='project-desc h-element'>
+                                        {attributes.description}
+                                    </div>
+
+                                    <div className='project-stack h-element'>
+                                        {attributes.stack}
+                                    </div>
+
+                                    <div className='project-links'>
+                                        <a target='_blank' href={attributes.github} ><AiOutlineGithub /></a>
+                                        <a target='_blank' href={attributes.web}><FiExternalLink /></a>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+
+        </>
     )
 }
 
