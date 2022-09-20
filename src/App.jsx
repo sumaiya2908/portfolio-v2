5ac021614e3536ee68f1ea84a9a10a8826237ffa
unknown <sumaiyahas2908@gmail.com>
2022-09-20 13:36:06 +0530

    feat: add about and project route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Home from './components/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CodingBook from './components/CodingBook';
 import CodingBookTitle from './components/CodingBookTitle';
+import About from './components/About';
+import Projects from './components/Projects';
+
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route exact path='/' element={<Home />}> </Route>
 				<Route path='/home' element={<Home />}> </Route>
+				<Route path='/about' element={<About />}></Route>
+				<Route path='/projects' element={<Projects/>}></Route>
 				<Route path='/codebook' element={<CodingBook />}></Route>
 				<Route path='/codebook/:title' element={<CodingBookTitle />}></Route>
 			</Routes>
