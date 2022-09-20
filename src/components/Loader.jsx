aa9dd8c7fd665218a3ccfd86a89ebfe67e29d929
unknown <sumaiyahas2908@gmail.com>
2022-09-20 13:40:26 +0530

    feat: make loader components

---
+++ src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Loader() {
+    return (
+        <div className="loader">
+            <span className="dot"></span>
+            <div className="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
